d5c0694b0196c30de6d4541bca3cd49d47512efc
Riya Joshi <riyajoshi312@gmail.com>
2022-06-07 18:40:59 +0530

    Virtual host setup, get invitee details

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class HomePageController extends AbstractController
 {
@@ -14,5 +18,47 @@ class HomePageController extends AbstractController
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController'
         ]);
+    }
+
+    #[Route('/checkout', name: 'app_checkout')]
+    public function checkout(Request $request, $stripeSK): Response
+    {
+        Stripe::setApiKey($stripeSK);
+
+        // $product_name = $request->request->get('product');
+        // $unit_amount = ($request->request->get('unitPrice')) * 100;
+        // $quantity = $request->request->get('quantity');
+
+        $session = Session::create([
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'inr',
+                'product_data' => [
+                  'name' => 'Expert Session',
+                ],
+                'unit_amount' => 20000,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+          ]);
+
+          // dd($session);
+          // return $response->withHeader('Location', $session->url)->withStatus(303);
+          return $this->redirect($session->url, 303);
+    }
+
+    #[Route('/success-url', name: 'success_url')]
+    public function successUrl(): Response
+    {
+        return $this->render('home_page/success.html.twig', []);
+    }
+
+    #[Route('/cancel-url', name: 'cancel_url')]
+    public function cancelUrl(): Response
+    {
+        return $this->render('home_page/cancel.html.twig', []);
     }
 }
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $name = $request->get('invitee_full_name');
+        $assignedTo = $request->get('assigned_to');
+        $eventType = $request->get('event_type_name');
+
         return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
+            'name' => $name,
+            'host' => $assignedTo,
+            'event' => $eventType
         ]);
     }
 }
