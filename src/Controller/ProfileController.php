e518cb2b2de799911b28c4f2df54e4aa47acad51
Riya Joshi <riyajoshi312@gmail.com>
2022-06-09 17:30:38 +0530

    Get All Events of Host - Part 1

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,24 +12,11 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(Request $request,CalendlyApiClient $calendly): Response
+    public function index(Request $request, CalendlyApiClient $calendly): Response
     {
        
-        $user = $calendly->fetchUserProfile();
+        $user = $calendly->getCurrentUserProfile();
         // dd($user);
-//         ProfileController.php on line 19:
-// array:10 [▼
-//   "avatar_url" => "https://d3v0px0pttie1i.cloudfront.net/uploads/user/avatar/18656265/bd85d019.jpg"
-//   "created_at" => "2022-05-30T05:17:16.190519Z"
-//   "current_organization" => "https://api.calendly.com/organizations/ad4e110a-da99-4d1c-95b0-51845c01ca13"
-//   "email" => "riyajoshi.190673107028@gmail.com"
-//   "name" => "Riya Joshi"
-//   "scheduling_url" => "https://calendly.com/riyajoshi-sal"
-//   "slug" => "riyajoshi-sal"
-//   "timezone" => "Asia/Calcutta"
-//   "updated_at" => "2022-06-07T06:28:41.042008Z"
-//   "uri" => "https://api.calendly.com/users/7744d9d1-5656-4d15-a974-1fdc81433b2a"
-// ]
         // $name = $user['name'];
         // var_dump('Name:' . $name);
 
@@ -40,15 +27,16 @@ class ProfileController extends AbstractController
         ]);
     }
 
-    #[Route('/my-schedule', name: 'app_my_schedule')]
-    public function mySchedule(Request $request): Response
+    #[Route('/my-events', name: 'app_my_events')]
+    public function myEvents(Request $request, CalendlyApiClient $calendly): Response
     {
-        $name = $request->get('invitee_full_name');
-        $assignedTo = $request->get('assigned_to');
-        $eventType = $request->get('event_type_name');
+        $user = $calendly->getCurrentUserProfile();
+        $events = $calendly->getAllEvents($user['uri']);
+        // dd($events);
 
-        return $this->render('profile/profile.html.twig', [
-            'controller_name' => 'ProfileController'
+        return $this->render('profile/myEvents.html.twig', [
+            'controller_name' => 'EventsController',
+            'events' => $events
         ]);
     }
 }
--- src/Http/CalendlyApiClient.php
+++ src/Http/CalendlyApiClient.php
@@ -16,10 +16,22 @@ class CalendlyApiClient
         $this->client = $calClient;
     }
     
-    public function fetchUserProfile() {
+    public function getCurrentUserProfile() {
         // dd($calendlyToken);
         $response = $this->client->request('GET', 'https://api.calendly.com/users/me', []); 
         return $response->toArray()['resource'];
+        // $headers = $response->getHeaders();
+        // dd($response->toArray());
+    }
+
+    public function getAllEvents($uri) {
+        // dd($uri);
+        $response = $this->client->request('GET', 'https://api.calendly.com/event_types', [
+            'query' => [
+                'user' => $uri
+            ]
+        ]); 
+        return $response->toArray()['collection'];
         // $headers = $response->getHeaders();
         // dd($response->toArray());
     }
