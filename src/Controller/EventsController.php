7c05f0d8ccd204507d33ee39548e1e860dfe9366
Riya Joshi <riyajoshi312@gmail.com>
2022-06-13 19:14:23 +0530

    Event Types and Scheduled Events

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EventsController extends AbstractController
 {
-    // My All Events (Both Active and Inactive)
+    // My All Events (Mix of Both Active and Inactive)
     #[Route('/my-events', name: 'app_my_events')]
     public function myEvents(Request $request, CalendlyApiClient $calendly): Response
     {
@@ -28,7 +28,8 @@ class EventsController extends AbstractController
         return $this->render('events/myEvents.html.twig', [
             'controller_name' => 'EventsController',
             'pager' => $pagerfanta,
-            'event_counts' => $event_counts
+            'event_counts' => $event_counts,
+            'slicer' => 37
         ]);
     }
 
@@ -40,6 +41,7 @@ class EventsController extends AbstractController
         $user = $calendly->getCurrentUserProfile();
         $events = $calendly->getSelectedEvents($user['uri'], $status);
         $event_counts = count($events);
+        // $pagerfanta = makePager($request, $events);
         $adapter = new ArrayAdapter($events);
         $pagerfanta = new Pagerfanta($adapter);
         $pagerfanta->setMaxPerPage(5);
@@ -48,11 +50,12 @@ class EventsController extends AbstractController
         return $this->render('events/myEvents.html.twig', [
             'controller_name' => 'EventsController',
             'pager' => $pagerfanta,
-            'event_counts' => $event_counts
+            'event_counts' => $event_counts,
+            'slicer' => 37
         ]);
     }
 
-    // Show Particular Event Details ..............................  STILL REMAINING TO IMPLEMENT IT
+    // Show Particular Event Details
     #[Route('/events/{uuid}', name: 'app_show_event')]
     public function showEvent($uuid, Request $request, CalendlyApiClient $calendly): Response
     {
@@ -66,22 +69,111 @@ class EventsController extends AbstractController
             'event' => $event
         ]);
     }
-
-        // Show Particular Event Details
-        #[Route('/scheduled-events', name: 'app_scheduled_events')]
-        public function scheduledEvents(Request $request, CalendlyApiClient $calendly): Response
-        {
-            $user = $calendly->getCurrentUserProfile();
-            // dd($slug);
-            $events = $calendly->getScheduledEvent($user['uri']);
-            // dd($events);
-            // $adapter = new ArrayAdapter($events);
-            // $pagerfanta = new Pagerfanta($adapter);
-            // $pagerfanta->setMaxPerPage(5);
-            // $pagerfanta->setCurrentPage($request->query->get('page', 1));
+    
+    // Show Scheduled Event Details
+    #[Route('/scheduled-events', name: 'app_scheduled_events')]
+    public function scheduledEvents(Request $request, CalendlyApiClient $calendly): Response
+    {
+        $user = $calendly->getCurrentUserProfile();
+        // dd($slug);
+        $events = $calendly->getAllScheduledEvents($user['uri']);
+        $event_counts = count($events);
+        // dd($events);    
+        // $pagerfanta = makePager($request, $events);
+        $adapter = new ArrayAdapter($events);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(5);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
            
-            return $this->render('events/showEvent.html.twig', [
-                'controller_name' => 'EventsController',
-            ]);
+        return $this->render('events/scheduled_events/myScheduledEvents.html.twig', [
+            'controller_name' => 'EventsController',
+            'pager' => $pagerfanta,
+            'event_counts' => $event_counts,
+            'slicer' => 42
+        ]);
+    }
+
+    // My Status Based Selected Events (Either Active Or Inactive)
+    #[Route('/scheduled-events/{status<0|1>}', name: 'app_selected_scheduled_events')]
+    public function mySelecetdScheduledEvents($status, Request $request, CalendlyApiClient $calendly): Response
+    {
+        // dd($status);
+        $user = $calendly->getCurrentUserProfile();
+        if ($status == 0) {
+            $status = "canceled";
         }
+        else {
+            $status = "active";
+        }
+
+        $events = $calendly->getSelectedScheduledEvents($user['uri'], $status);
+        $event_counts = count($events);
+        // $pagerfanta = makePager($request, $events);
+        $adapter = new ArrayAdapter($events);
+        $pagerfanta = new Pagerfanta($adapter);
+        $pagerfanta->setMaxPerPage(5);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
+        
+        return $this->render('events/scheduled_events/myScheduledEvents.html.twig', [
+            'controller_name' => 'EventsController',
+            'pager' => $pagerfanta,
+            'event_counts' => $event_counts,
+            'slicer' => 42
+        ]);
+    }
+
+    // Show Particular Scheduled Event Details
+    #[Route('/scheduled-events/{uuid}', name: 'app_show_scheduled_event')]
+    public function showScheduledEvent($uuid, Request $request, CalendlyApiClient $calendly): Response
+    {
+        $user = $calendly->getCurrentUserProfile();
+        // dd($uuid);
+        $event = $calendly->getParticularScheduledEvent($uuid);
+        // dd($event);
+               
+        return $this->render('events/scheduled_events/showScheduledEvent.html.twig', [
+            'controller_name' => 'EventsController',
+            'event' => $event,
+            'slicer' => 42
+        ]);
+    }
+
+    // Show Invitees of Scheduled Event based on Event UUID
+    #[Route('/scheduled-events/{uuid}/invitees', name: 'app_show_scheduled_event_invitees')]
+    public function showScheduledEventInvitees($uuid, Request $request, CalendlyApiClient $calendly): Response
+    {
+        $user = $calendly->getCurrentUserProfile();
+        // dd($uuid);
+        $invitees = $calendly->getParticularScheduledEventInvitees($uuid);
+        // dd($invitees);
+        $event_name = $calendly->getParticularScheduledEventName($uuid);
+        // dd($event_name);
+               
+        return $this->render('events/scheduled_events/showScheduledEventInvitees.html.twig', [
+            'controller_name' => 'EventsController',
+            'invitees' => $invitees,
+            'event_name' => $event_name,
+            'uuid' => $uuid
+        ]);
+    }
+
+    // Show Invitees of Scheduled Event based on Event UUID and Invitee UUID
+    #[Route('/scheduled-events/{uuid}/invitees/{uid}', name: 'app_show_scheduled_event_invitees_by_uid')]
+    public function showScheduledEventInviteesByUId($uuid, $uid, Request $request, CalendlyApiClient $calendly): Response
+    {
+        $user = $calendly->getCurrentUserProfile();
+        // dd($uuid);
+        $invitees = $calendly->getParticularScheduledEventInviteesByUId($uuid, $uid);
+        dd($invitees);
+        $event_name = $calendly->getParticularScheduledEventName($uuid);
+        // dd($event_name);
+               
+        return $this->render('events/scheduled_events/showScheduledEventInviteesByUid.html.twig', [
+            'controller_name' => 'EventsController',
+            'invitees' => $invitees,
+            'event_name' => $event_name,
+            'uuid' => $uuid,
+            'uid' => $uid
+        ]);
+    }
 }
--- src/Http/CalendlyApiClient.php
+++ src/Http/CalendlyApiClient.php
@@ -8,22 +8,27 @@ class CalendlyApiClient
 {   
     // private const URL = 'https://api.calendly.com';
     private $client;
+    private int $count = 100;
+
     public function __construct(HttpClientInterface $calClient)
     {
         $this->client = $calClient;
     }
     
+    // Get Current User Profile
     public function getCurrentUserProfile() {
         $response = $this->client->request('GET', 'https://api.calendly.com/users/me', []); 
         return $response->toArray()['resource'];
         // dd($response->toArray());
     }
 
+    // Get All Events of the User
     public function getAllEvents($uri) {
         // dd($uri);
         $response = $this->client->request('GET', 'https://api.calendly.com/event_types', [
             'query' => [
-                'user' => $uri
+                'user' => $uri,
+                'count' => $this->count
             ]
         ]);
         
@@ -31,19 +36,22 @@ class CalendlyApiClient
         // dd($response->toArray());
     }
 
+    // Get Active/ Inactive Events of the User
     public function getSelectedEvents($uri, $status) {
         // dd($uri);
         // dd($status);
         $response = $this->client->request('GET', 'https://api.calendly.com/event_types', [
             'query' => [
                 'user' => $uri,
-                'active' => $status
+                'active' => $status,
+                'count' => $this->count
             ]
         ]); 
         
         return $response->toArray()['collection'];
     }
    
+    // Get Details of particular Event based on Event uuid
     public function getParticularEvent($uuid) {
         //dd($uuid);
         $response = $this->client->request('GET', 'https://api.calendly.com/event_types/'.$uuid.'', [
@@ -53,11 +61,67 @@ class CalendlyApiClient
         ]);
         
         return $response->toArray()['resource'];
-        // dd($response->toArray()['resource']);
     }
 
-    public function getScheduledEvent()
+    // Get All Events of the User
+    public function getAllScheduledEvents($uri) {
+        // dd($uri);
+        $response = $this->client->request('GET', 'https://api.calendly.com/scheduled_events', [
+            'query' => [
+                'user' => $uri,
+                'count' => $this->count
+            ]
+        ]);
+        
+        return $response->toArray()['collection'];
+    }
+
+    public function getSelectedScheduledEvents($uri, $status)
     {
-        # code...
+        $response = $this->client->request('GET', 'https://api.calendly.com/scheduled_events', [
+            'query' => [
+                'user' => $uri,
+                'status' => $status,
+                'count' => $this->count
+            ]
+        ]); 
+
+        return $response->toArray()['collection'];
+    }
+
+    // Get Details of particular Scheduled Event based on Event uuid
+    public function getParticularScheduledEvent($uuid) {
+        //dd($uuid);
+        $response = $this->client->request('GET', 'https://api.calendly.com/scheduled_events/'.$uuid.'', [
+        ]);
+        
+        return $response->toArray()['resource'];
+    }
+
+    // Get Details of particular Scheduled Event based on Event uuid
+    public function getParticularScheduledEventInvitees($uuid) {
+        //dd($uuid);
+        $response = $this->client->request('GET', 'https://api.calendly.com/scheduled_events/'.$uuid.'/invitees', [
+            'query' => [
+                'count' => $this->count
+            ]
+        ]);
+        
+        return $response->toArray()['collection'];
+    }
+
+    public function getParticularScheduledEventName($uuid) {
+        return $this->getParticularScheduledEvent($uuid)['name'];
+    }
+
+    public function getParticularScheduledEventInviteesByUId($uuid, $uid) {
+        $response = $this->client->request('GET', 'https://api.calendly.com/scheduled_events/'.$uuid.'/invitees/'.$uid, [
+            'query' => [
+                'count' => $this->count
+            ]
+        ]);
+        
+        // return $response->toArray()['resource'];
+        dd($response->toArray()['resource']);
     }
 }
